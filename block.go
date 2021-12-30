00fd08888c24bb6aac456302663daef56a03f1fe
Sergey <xxsmotur@gmail.com>
2021-12-30 23:18:55 +0500

    Add methods for creating blocks

--- block.go
+++ block.go
@@ -7,6 +7,12 @@ type Block struct {
 	Nonce int
 }
 
+func CreateBlock(data, prevHash []byte) *Block {
+    block := &Block{[]byte{}, data, prevHash}
+    block.Hashing()
+    return block
+}
+
 func (b *Block) Hashing() {
 	info := bytes.Join([][]byte{b.Data, b.PrevHash}, []byte{})
 	hash := sha256.Sum256(info)
@@ -15,4 +21,10 @@ func (b *Block) Hashing() {
 
 type BlockChain struct {
 	Blocks []*Block
+}
+
+func (chain *BlockChain) AddBlock(data []byte) {
+    prevBlock := chain.blocks[len(chain.blocks)-1]
+    new := CreateBlock(data, prevBlock.Hash)
+    chain.blocks = append(chain.blocks, new)
 }
