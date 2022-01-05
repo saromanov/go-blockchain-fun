3248bf08cc627eb4b9269ed5998ee4bc0b060886
Sergey <xxsmotur@gmail.com>
2022-01-05 17:32:12 +0500

    Update pow algorithm

--- pow.go
+++ pow.go
@@ -1,5 +1,10 @@
 package blockchain
 
+import (
+    "bytes"
+    "math"
+    "math/big"
+)
 // ProofOfWork defines structure for proof of work
 type ProofOfWork struct {
 	Block *Block
@@ -12,4 +17,41 @@ func NewProofOfWork(b *Block) *ProofOfWork {
     target.Lsh(target, uint(256-Difficulty))
     pow := &ProofOfWork{b, target}
     return pow
+}
+
+func (pow *ProofOfWork) MakeNonce(nonce, difficylty int64) []byte {
+    data := bytes.Join(
+        [][]byte{
+            pow.Block.PrevHash,
+            pow.Block.Data,
+            ToHex(nonce),
+            ToHex(difficulty),
+        },
+        []byte{},
+    )
+    return data
+}
+
+func (pow *ProofOfWork) Run() (int, []byte) {
+    var intHash big.Int
+    var hash [32]byte
+
+    nonce := 0
+    for nonce < math.MaxInt64 {
+        data := pow.InitNonce(nonce)
+        hash = sha256.Sum256(data)
+
+        fmt.Printf("\r%x", hash)
+        intHash.SetBytes(hash[:])
+
+        if intHash.Cmp(pow.Target) == -1 {
+            break
+        } else {
+            nonce++
+        }
+
+    }
+    fmt.Println()
+
+    return nonce, hash[:]
 }
