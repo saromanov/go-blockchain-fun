e2b0b12a52f712eeac0e968f1b86c81454498360
Sergey <xxsmotur@gmail.com>
2022-01-08 23:58:52 +0500

    Add comments

--- blockchain.go
+++ blockchain.go
@@ -4,12 +4,14 @@ type BlockChain struct {
 	Blocks []*Block
 }
 
+// AddBlock provides adding of the new block to blockchain
 func (chain *BlockChain) AddBlock(data []byte) {
     prevBlock := chain.Blocks[len(chain.Blocks)-1]
     new := CreateBlock(data, prevBlock.Hash)
     chain.Blocks = append(chain.Blocks, new)
 }
 
+// Create provides creating of the new block with genesis
 func Create()*BlockChain {
 	return &BlockChain{[]*Block{Genesis()}}
 }
